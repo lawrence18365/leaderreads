e01f6f24cb210fbd345437c27ba214252bf6adeb
Lawrence <lawrencebrennan@gmail.com>
2024-10-03 18:28:23 +1000

    Merge remote changes and resolve conflicts

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-      unoptimized: true,
-      domains: ['www.leaderreads.info'],
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+    domains: ['leaderreads.info'], // Remove 'www.' if you're not using it
+  },
+  // Add this line:
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+}
+
+module.exports = nextConfig
